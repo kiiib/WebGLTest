c9e3031c0733a7adcd8b3033bed8c4980fef9286
Jianbin Liu <billyljb@gmail.com>
2018-02-26 23:26:11 +0800

    Add attribute type variable, a_PointSize
    Add attribute type variable, a_PointSize

--- HelloPoint2.js
+++ HelloPoint2.js
@@ -2,9 +2,10 @@
 // Vertex shading
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
+    'attribute float a_PointSize;\n' +
     'void main() {\n' +
     '   gl_Position = a_Position;\n' +
-    '   gl_PointSize = 10.0;\n' +
+    '   gl_PointSize = a_PointSize;\n' +
     '}\n';    
 
 // Fragment shading
@@ -34,8 +35,11 @@ function main() {
         console.log('Failed to get the storage location of a_Position');
     }
 
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+
     // transport the vertex location to variable 'attribute'
-    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+    gl.vertexAttrib3f(a_Position, 0.3, 0.0, 0.0);
+    gl.vertexAttrib1f(a_PointSize, 15.0);
 
     // Set <canvas> bg color
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
